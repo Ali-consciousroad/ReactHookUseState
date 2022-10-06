4f864bcccb862ae42e11686bc02e0b6a7d756edb
Ali-consciousroad <ali_dindar@live.be>
2022-10-06 18:11:05 +0200

    Update our state properly using a function when we need to update our state based on the previous state value

--- src/App.js
+++ src/App.js
@@ -7,16 +7,25 @@ function App() {
   - Most of the time we'll see the useState array using destructuring 
   - The first value is the default value, the second one is the function used
   to modify the default value, update our current state
-  - The value inside useState() give the default value
+  - The value inside useState() give the default valueto 
    */
   const [count, setCount] = useState(21)
 
+  // Every time we call our setState() function, the component will rerender like setState() used with class-based components
   function decrementCount(){
-    setCount(count - 1)
+    // This will cause issue if we call setCount() 2 time in a row, the function will override each other and decrement our default value only once! 
+    // setCount(count - 1)
+    
+    // Everytime we use the previous value of our state to update it we need to use a function inside setCount() fct. 
+    // Same principles apply with setState() in class component.
+    setCount(prevCount => prevCount - 1)
+    setCount(prevCount => prevCount - 1)
   }
 
   function incrementCount(){
-    setCount(count +1)
+    // setCount(count +1)
+    setCount(prevCount => prevCount + 1)
+    setCount(prevCount => prevCount + 1)
   }
 
   return (
