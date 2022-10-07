8bd7ea46c366da212908a7bd7b38c0912f3574ce
Ali-consciousroad <ali_dindar@live.be>
2022-10-07 17:24:30 +0200

    Show how to update the state and keep the old values

--- src/App.js
+++ src/App.js
@@ -7,41 +7,42 @@ function countInitial() {
 }
 
 function App() {
-  /* 
+    /* 
   - useState is an array with 2 values 
   - Most of the time we'll see the useState array using destructuring 
   - The first value is the default value, the second one is the function used
   to modify the default value, update our current state'
-  - The value inside useState() give the default valueto 
+  - The merge of old values doesn't not autmatically happen because in general with useState() hook,
+  we'll prefere to use seperate hook for each value! 
    */
   
-  /* This will call the default value each time the component is rerendered
-  and this can cause big issue when the default value is a big value. Ex. Fibonacci number */
-  // const [count, setCount] = useState(21) // This will cause performance issue 
-  // By using a function inside our useState() hook, the default value is called only once, the first time the component is called.
-  const [count, setCount] = useState(() => countInitial())
+  const [state, setState] = useState({ count: 4, theme: "red" })
+  const count = state.count
+  const theme = state.theme
 
-  // Every time we call our setState() function, the component will rerender like setState() used with class-based components
   function decrementCount(){
-    // This will cause issue if we call setCount() 2 time in a row, the function will override each other and decrement our default value only once! 
-    // setCount(count - 1)
-    
-    // Everytime we use the previous value of our state to update it we need to use a function inside setCount() fct. 
-    // Same principles apply with setState() in class component.
-    setCount(prevCount => prevCount - 1)
-    setCount(prevCount => prevCount - 1)
+    setState(prevState => {
+      /* 
+      How this work in class-based component but this won't work properly 
+      because instead of merging the old state with the new one, with function-based component, 
+      the old state is totally overwritten! To avoid this when working with objects,
+      we need to use to spread out the old state and keep all the old values. 
+      */
+
+      //return { count: prevState.count - 1 } // Overwrite the old values!
+      return { ...prevState, count: prevState.count - 1 }
+    })
   }
 
   function incrementCount(){
-    // setCount(count +1)
-    setCount(prevCount => prevCount + 1)
-    setCount(prevCount => prevCount + 1)
+    // setCount(prevCount => prevCount + 1)
   }
 
   return (
     <>
       <button onClick={decrementCount}>-</button>
       <span>{count}</span>
+      <span>{theme}</span>
       <button onClick={incrementCount}>+</button>
     </>
   );
